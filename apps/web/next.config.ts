fbee18b8076f8ebbf2a73f2416a2d5ddf6173668
shahbaz-athwal <shahbazathwal2107@gmail.com>
2026-01-24 18:47:42 -0400

    init

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@job-analyzer/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	typedRoutes: true,
+	reactCompiler: true,
+};
+
+export default nextConfig;
